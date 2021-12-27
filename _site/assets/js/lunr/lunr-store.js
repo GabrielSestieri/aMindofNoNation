fb6b9fd747591d41da494c9be818f002b7c48340
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2021-12-27 03:51:33 -0500

    changed href

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,6 +5,12 @@ var store = [{
         "url": "/writeups/basicctf/2021/12/22/basicctf.html",
         "teaser": null
       },{
+        "title": "hangman.py",
+        "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","hangman"],
+        "tags": [],
+        "url": "/projects/hangman/2021/12/22/hangman.html",
+        "teaser": null
+      },{
         "title": "Basic CTF",
         "excerpt":"Enumeration   Nmap  We use nmap -A -vv -Pn IP   GoBuster  gobuster dir … … IP … …   Brute Forcing / Cracking   Hydra   John the Ripper  ","categories": ["writeups","solr"],
         "tags": [],
