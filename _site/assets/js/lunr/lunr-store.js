263c110389deb118f4d980c1d65fde033be3b709
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2021-12-27 00:15:33 -0500

    changed projects

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "Basic CTF",
         "excerpt":"Enumeration   Nmap  We use nmap -A -vv -Pn IP   GoBuster  gobuster dir … … IP … …   Brute Forcing / Cracking   Hydra   John the Ripper  ","categories": ["writeups","basicctf"],
         "tags": [],
-        "url": "/aMindofNoNation/writeups/basicctf/2021/12/22/basicctf.html",
+        "url": "/writeups/basicctf/2021/12/22/basicctf.html",
         "teaser": null
       },{
         "title": "connect4.py",
         "excerpt":"\r ","categories": ["projects","connect4"],
         "tags": [],
-        "url": "/aMindofNoNation/projects/connect4/2021/12/22/connect4.html",
+        "url": "/projects/connect4/2021/12/22/connect4.html",
         "teaser": null
       },{
         "title": "hangman.py",
         "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","hangman"],
         "tags": [],
-        "url": "/aMindofNoNation/projects/hangman/2021/12/22/hangman.html",
+        "url": "/projects/hangman/2021/12/22/hangman.html",
         "teaser": null
       },{
         "title": "Basic CTF",
-        "excerpt":" ","categories": ["writeups","basicctf"],
+        "excerpt":"Enumeration   Nmap  We use nmap -A -vv -Pn IP   GoBuster  gobuster dir … … IP … …   Brute Forcing / Cracking   Hydra   John the Ripper  ","categories": ["writeups","solr"],
         "tags": [],
-        "url": "/aMindofNoNation/writeups/basicctf/2021/12/22/solr.html",
+        "url": "/writeups/solr/2021/12/22/solr.html",
         "teaser": null
       },{
         "title": "basketball_predictor.py",
         "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","basketball"],
         "tags": [],
-        "url": "/aMindofNoNation/projects/basketball/2021/12/24/basketball.html",
+        "url": "/projects/basketball/2021/12/24/basketball.html",
         "teaser": null
       },{
         "title": "basketball_predictor.py",
         "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","blackjack"],
         "tags": [],
-        "url": "/aMindofNoNation/projects/blackjack/2021/12/24/blackjack.html",
+        "url": "/projects/blackjack/2021/12/24/blackjack.html",
         "teaser": null
       }]
