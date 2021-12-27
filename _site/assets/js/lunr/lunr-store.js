f5bf94c8e6fda65ad5e18d6dc593b5bdfaa8c9b7
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2021-12-27 03:48:50 -0500

    forgot to update the date

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,46 +5,10 @@ var store = [{
         "url": "/writeups/basicctf/2021/12/22/basicctf.html",
         "teaser": null
       },{
-        "title": "connect4.py",
-        "excerpt":" Video Demonstration   \r \r The Code","categories": ["projects","connect4"],
-        "tags": [],
-        "url": "/projects/connect4/2021/12/22/connect4.html",
-        "teaser": null
-      },{
-        "title": "hangman.py",
-        "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","hangman"],
-        "tags": [],
-        "url": "/projects/hangman/2021/12/22/hangman.html",
-        "teaser": null
-      },{
         "title": "Basic CTF",
         "excerpt":"Enumeration   Nmap  We use nmap -A -vv -Pn IP   GoBuster  gobuster dir … … IP … …   Brute Forcing / Cracking   Hydra   John the Ripper  ","categories": ["writeups","solr"],
         "tags": [],
         "url": "/writeups/solr/2021/12/22/solr.html",
-        "teaser": null
-      },{
-        "title": "drug_interactions.py",
-        "excerpt":" Video Demonstration   \r \r The Code","categories": ["projects","druginter"],
-        "tags": [],
-        "url": "/projects/druginter/2021/12/22/drunginter.html",
-        "teaser": null
-      },{
-        "title": "enron.py",
-        "excerpt":" Video Demonstration   \r \r The Code","categories": ["projects","enron"],
-        "tags": [],
-        "url": "/projects/enron/2021/12/22/enron.html",
-        "teaser": null
-      },{
-        "title": "keylogger.py",
-        "excerpt":" Video Demonstration   \r \r The Code","categories": ["projects","keylogger"],
-        "tags": [],
-        "url": "/projects/keylogger/2021/12/22/keylogger.html",
-        "teaser": null
-      },{
-        "title": "robot.py",
-        "excerpt":" Video Demonstration   \r \r The Code","categories": ["projects","robot"],
-        "tags": [],
-        "url": "/projects/robot/2021/12/22/robot.html",
         "teaser": null
       },{
         "title": "basketball_predictor.py",
