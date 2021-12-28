5e2f8593a63e5638d19417062b3710c825efc61c
GabrielSestieri <63177325+GabrielSestieri@users.noreply.github.com>
2021-12-28 00:53:13 -0500

    added project descriptions

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "hangman.py",
-        "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","hangman"],
+        "excerpt":"\r     \r          Video Demonstration   \r         \r     \r     \r         Description  \r         A simple hangman game that is played through the terminal. \r         The Code\r     \r ","categories": ["projects","hangman"],
         "tags": [],
         "url": "/projects/hangman/2021/12/22/hangman.html",
         "teaser": null
@@ -18,44 +18,56 @@ var store = [{
         "teaser": null
       },{
         "title": "basketball_predictor.py",
-        "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","basketball"],
+        "excerpt":"Video Demonstration Description Runs by using the command line; a user can input two NBA teams after calling the program and the program will output several bar graphs comparing team stats and a pie chart predicting who would win between the two teams. Based on data from the 2018 NBA...","categories": ["projects","basketball"],
         "tags": [],
         "url": "/projects/basketball/2021/12/24/basketball.html",
         "teaser": null
       },{
         "title": "basketball_predictor.py",
-        "excerpt":" Video Demonstration   \r \r The Code\r ","categories": ["projects","blackjack"],
+        "excerpt":"\r     \r          Video Demonstration   \r         \r     \r     \r         Description  \r         Runs by using the command line; a user can choose the amount they want to bet and then wether to hit (add a card) or stand (satisfied with the cards you have). By playing another hand your score carries over to the next round. \r         The Code\r     \r ","categories": ["projects","blackjack"],
         "tags": [],
         "url": "/projects/blackjack/2021/12/24/blackjack.html",
         "teaser": null
       },{
         "title": "connect4.py",
-        "excerpt":" Video Demonstration     The Code","categories": ["projects","connect4"],
+        "excerpt":"               Video Demonstration                               Description           Runs by using the command line; A terminal based connect 4 game. Can run more than two players.          The Code      ","categories": ["projects","connect4"],
         "tags": [],
         "url": "/projects/connect4/2021/12/27/connect4.html",
         "teaser": null
       },{
         "title": "drug_interactions.py",
-        "excerpt":" Video Demonstration     The Code   ","categories": ["projects","druginter"],
+        "excerpt":"Video Demonstration Description drug_interactions.py is a Python program that makes a series of calls to the National Library of Medicine's REST API. It then collects all interactions between two drugs. For example if a user inputs \"Insulin\" the program outputs a file called interactions.txt that stores all interactions between insulin...","categories": ["projects","druginter"],
         "tags": [],
         "url": "/projects/druginter/2021/12/27/druginter.html",
         "teaser": null
       },{
         "title": "enron.py",
-        "excerpt":" Video Demonstration     The Code","categories": ["projects","enron"],
+        "excerpt":"Video Demonstration Description Runs by using the command line; a user can input any keyword they'd like to search through the Enron email database. Uses regex and classes to clean and classify the dataset in order to then be able to call specific parts that a user might need. The...","categories": ["projects","enron"],
         "tags": [],
         "url": "/projects/enron/2021/12/27/enron.html",
         "teaser": null
       },{
         "title": "keylogger.py",
-        "excerpt":" Video Demonstration     The Code","categories": ["projects","keylogger"],
+        "excerpt":"Video Demonstration Description A simple Python based keylogger that uses pynput to record keystrokes and then store them in a raw and cleaned file where the cleaned file has certain function keys (backspace, enter, etc) edited to represent their function. I plan to add a functionality that automatically sends the...","categories": ["projects","keylogger"],
         "tags": [],
         "url": "/projects/keylogger/2021/12/27/keylogger.html",
         "teaser": null
       },{
         "title": "robot.py",
-        "excerpt":" Video Demonstration     The Code","categories": ["projects","robot"],
+        "excerpt":"               Video Demonstration                               Description           A class project that introduced me to OOP in Python. Simulates a very simple fight between two different classes.          The Code      ","categories": ["projects","robot"],
         "tags": [],
         "url": "/projects/robot/2021/12/27/robot.html",
+        "teaser": null
+      },{
+        "title": "gplayscrape.py",
+        "excerpt":"Video Demonstration Description I was tasked with finding a way to scrape information on app developers published on Google Play based on specific keywords. Fortunately I found a pip package that established a foundation that I could build upon. Using Selenium I continuously parse through the page until it reaches...","categories": ["projects","gplayscrape"],
+        "tags": [],
+        "url": "/projects/gplayscrape/2021/12/28/gplayscrape.html",
+        "teaser": null
+      },{
+        "title": "linkedinbot.py",
+        "excerpt":"Video Demonstration Description After the GooglePlay Scraper I was tasked with a LinkedIn bot that could automatically connect with a person based on the searched keyword. It would then send a prebuilt personalized note to aid in the recruiting process. This program runs only after providing the the GUI with...","categories": ["projects","linkedinbot"],
+        "tags": [],
+        "url": "/projects/linkedinbot/2021/12/28/linkedinbot.html",
         "teaser": null
       }]
